7aeb86be9f9507de76fe48ad5ceda0ee59363528
Manuel Roncero <mroncerogago@gmail.com>
2025-04-29 18:31:37 +0200

    STT ENDED

--- Lab4/STT_APY.py
+++ Lab4/STT_APY.py
@@ -2,8 +2,8 @@ import assemblyai as aai
 
 aai.settings.api_key = "3bc98b278aee480c94f1c77834bf1556"
 
-# audio_file = "./local_file.mp3"
-audio_file = r"C:\Users\manue\OneDrive\Documentos\Universidad\NLP lunes\NLP_DIGITAL_PORTFOLIO\Lab4\elevenlabs_tts_output.mp3"
+
+audio_file = r"C:\Users\manue\OneDrive\Documentos\Universidad\NLP lunes\NLP_DIGITAL_PORTFOLIO\elevenlabs_tts_output.mp3"
 
 config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.best)
 
